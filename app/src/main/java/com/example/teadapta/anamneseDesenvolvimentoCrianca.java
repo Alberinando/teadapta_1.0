5271dcf7fcbaad1d544dedcccbaf49d6d679c9d1
alberinando <alberinandocipriano@hotmail.com>
2023-09-23 15:06:55 -0300

    Tela Anamnese - A Criança atualmente (Colocação da String)

--- app/src/main/java/com/example/teadapta/anamneseDesenvolvimentoCrianca.java
+++ app/src/main/java/com/example/teadapta/anamneseDesenvolvimentoCrianca.java
@@ -7,6 +7,8 @@ import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.RadioButton;
 
 public class anamneseDesenvolvimentoCrianca extends AppCompatActivity {
@@ -15,12 +17,59 @@ public class anamneseDesenvolvimentoCrianca extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anamnese_desenvolvimento_crianca);
+        AutoCompleteTextView escolha = findViewById(R.id.escolha);
+        AutoCompleteTextView escolha2 = findViewById(R.id.escolha2);
+        AutoCompleteTextView escolha3 = findViewById(R.id.escolha3);
+        AutoCompleteTextView escolha4 = findViewById(R.id.escolha4);
+        AutoCompleteTextView escolha5 = findViewById(R.id.escolha5);
+        AutoCompleteTextView escolha6 = findViewById(R.id.escolha6);
+        AutoCompleteTextView escolha7 = findViewById(R.id.escolha7);
+        AutoCompleteTextView escolha8 = findViewById(R.id.escolha8);
         RadioButton radioDestro = findViewById(R.id.radioDestro);
         RadioButton radioCanhoto = findViewById(R.id.radioCanhoto);
 
         radioDestro.setButtonTintList(ColorStateList.valueOf(Color.parseColor("#000000")));
         radioCanhoto.setButtonTintList(ColorStateList.valueOf(Color.parseColor("#000000")));
 
+        String[] listaEscolha = getResources().getStringArray(R.array.Escolha);
+        ArrayAdapter<String> adapterEscolha = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, listaEscolha);
+        escolha.setAdapter(adapterEscolha);
+
+        String[] listaEscolha2 = getResources().getStringArray(R.array.Escolha);
+        ArrayAdapter<String> adapterEscolha2 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, listaEscolha2);
+        escolha2.setAdapter(adapterEscolha2);
+
+        String[] listaEscolha3 = getResources().getStringArray(R.array.Escolha);
+        ArrayAdapter<String> adapterEscolha3 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, listaEscolha3);
+        escolha3.setAdapter(adapterEscolha3);
+
+        String[] listaEscolha4 = getResources().getStringArray(R.array.Escolha);
+        ArrayAdapter<String> adapterEscolha4 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, listaEscolha4);
+        escolha4.setAdapter(adapterEscolha4);
+
+        String[] listaEscolha5 = getResources().getStringArray(R.array.Escolha);
+        ArrayAdapter<String> adapterEscolha5 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, listaEscolha5);
+        escolha5.setAdapter(adapterEscolha5);
+
+        String[] listaEscolha6 = getResources().getStringArray(R.array.Escolha);
+        ArrayAdapter<String> adapterEscolha6 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, listaEscolha6);
+        escolha6.setAdapter(adapterEscolha6);
+
+        String[] listaEscolha7 = getResources().getStringArray(R.array.Escolha);
+        ArrayAdapter<String> adapterEscolha7 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, listaEscolha7);
+        escolha7.setAdapter(adapterEscolha7);
+
+        String[] listaEscolha8 = getResources().getStringArray(R.array.Escolha);
+        ArrayAdapter<String> adapterEscolha8 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, listaEscolha8);
+        escolha8.setAdapter(adapterEscolha8);
     }
     public void Back (View view){
         Intent intent = new Intent(anamneseDesenvolvimentoCrianca.this, com.example.teadapta.anamneseDesenvolvimento.class);
