bb644cdfa6eaaa3cc87aa0a459f80c68d3d06887
alberinando <alberinandocipriano@hotmail.com>
2023-10-19 00:14:00 -0300

    tela relatório

--- app/src/main/java/com/example/teadapta/ficha_de_acompanhamento.java
+++ app/src/main/java/com/example/teadapta/ficha_de_acompanhamento.java
@@ -3,12 +3,24 @@ package com.example.teadapta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class ficha_de_acompanhamento extends AppCompatActivity {
+
+    private AutoCompleteTextView escolha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ficha_de_acompanhamento);
+
+        escolha = findViewById(R.id.escolha);
+
+        //Escolha do Gênero
+        String[] listaEscolha = getResources().getStringArray(R.array.Genero);
+        ArrayAdapter<String> adapterEscolha = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, listaEscolha);
+        escolha.setAdapter(adapterEscolha);
     }
 }
